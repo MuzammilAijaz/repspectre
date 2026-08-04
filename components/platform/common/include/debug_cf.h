81ff5380c45241dd7e915740d39c111a77bdcf92
muz <171587778+MuzammilAijaz@users.noreply.github.com>
2026-08-04 14:53:28 +0500

    chore(debug): add compile-time guard to disable debug prints in non-debug builds

--- components/platform/common/include/debug_cf.h
+++ components/platform/common/include/debug_cf.h
@@ -1,10 +1,20 @@
 #ifndef DEBUG_CF_H
 #define DEBUG_CF_H
 
+#ifdef DEBUG_PRINTS_ENABLED
+
 #include <stdio.h>
 #define DEBUG_PRINT(format, ...) printf(format, ##__VA_ARGS__)
 #define DEBUG_PRINTI(format, ...) printf(format, ##__VA_ARGS__)
 #define DEBUG_PRINTE(format, ...) printf(format, ##__VA_ARGS__)
 #define DEBUG_PRINTD(format, ...) printf(format, ##__VA_ARGS__)
+
+#else
+#define DEBUG_PRINT(format, ...)
+#define DEBUG_PRINTI(format, ...)
+#define DEBUG_PRINTE(format, ...)
+#define DEBUG_PRINTD(format, ...)
+
+#endif
 
 #endif
